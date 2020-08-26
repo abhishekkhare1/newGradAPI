702e0f41d1e60c388c01c5fffa5fa6057c032a9c
Abhishek Khare <abhishek.khare@utexas.edu>
2020-08-26 01:07:14 -0500

    Checks if newer time and updates old time if newer

--- findjob.py
+++ findjob.py
@@ -4,7 +4,30 @@ import json
 
 URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/commits/master"
 
-r = requests.get(url = URL).json()
-with open('response.json', 'w') as outfile:
-    json.dump(r, outfile)
-print(r["commit"]["author"]["date"])
+def getNewDate():
+    response = requests.get(url = URL).json()
+    return str(response["commit"]["author"]["date"])
+
+
+def getOldDate():
+    f = open("date", "r")
+    oldDate = f.read()
+    f.close()
+    return oldDate
+
+def writeOldDate(newDate):
+    f = open("date", "w")
+    f.write(newDate)
+    f.close()
+
+def main():
+    newDate = getNewDate()
+    oldDate = getOldDate()
+    
+    if newDate > oldDate:
+        writeOldDate(newDate)
+        print("updated")
+
+
+main()
+
