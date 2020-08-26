16fce7ac92bcf34dacdd6d5eb1d6e338820f6b62
Abhishek Khare <abhishek.khare@utexas.edu>
2020-08-26 03:18:32 -0500

    Commented functions and cleaned up README

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # newGradAPI
 
 BIG SHOUTOUT to Pitt Computer Science Club for compiling a list of applications and regularly
-checking them, y'all saves us a lot of effort this recruitment cycle
+checking if new ones are open, y'all saved us a lot of effort this recruitment cycle
 
-Sends me a text message if a new 2021 SWE New Grad application has opened
+Polls https://github.com/Pitt-CSC/NewGrad-2021 to determine if a new commit has been made
 
-Polls https://github.com/Pitt-CSC/NewGrad-2021 to determine if new commit has been made
-
-Sends text using the Twilio API when new commit found
+If updated, sends text with company and url of new application using the Twilio API
--- findjob.py
+++ findjob.py
@@ -9,11 +9,30 @@ from twilio.rest import Client
 COMMIT_URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/commits/master"
 README_URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/readme"
 
-#GET /repos/:owner/:repo/git/blobs/:file_sha
+
+#Gets date of most recent commit from the repo
 def getNewDate():
     response = requests.get(url = COMMIT_URL).json()
     return str(response["commit"]["author"]["date"])
 
+
+#Reads file to find date from last polled commit
+def getOldDate():
+    f = open("date", "r")
+    oldDate = f.read()
+    f.close()
+    return oldDate
+
+
+#Writes date to file if newer
+def writeOldDate(newDate):
+    f = open("date", "w")
+    f.write(newDate)
+    f.close()
+
+
+#Makes a request to the repo readme
+#Parses repo to get company name and url to application
 def parseReadMe():
     response = requests.get(url = README_URL).json()
     readme = str(base64.b64decode(response['content']))
@@ -28,17 +47,8 @@ def parseReadMe():
     
     return company, link
 
-def getOldDate():
-    f = open("date", "r")
-    oldDate = f.read()
-    f.close()
-    return oldDate
 
-def writeOldDate(newDate):
-    f = open("date", "w")
-    f.write(newDate)
-    f.close()
-
+#Texts my number with a message using the Twilio API
 def sendText(message):
     
     if len(message) >= 160:
@@ -55,6 +65,8 @@ def sendText(message):
                      to=os.environ['MY_PHONE_NUMBER']                   
                  )
 
+
+#main
 def main():
     newDate = getNewDate()
     oldDate = getOldDate()
@@ -68,4 +80,3 @@ def main():
 
 
 main()
-
