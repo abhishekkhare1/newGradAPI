537fd083ab4ef5d47107544960812c12f125a431
Abhishek Khare <abhishek.khare@utexas.edu>
2020-08-26 01:43:00 -0500

    Sends text when date is updated

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # newGradAPI
 Sends me a text message if a new 2021 SWE New Grad application has opened
 
-Polls https://github.com/Pitt-CSC/NewGrad-2021 and sends text if a new commit with application has been added.
+Polls https://github.com/Pitt-CSC/NewGrad-2021 to determine if new commit has been made
+
+Sends text using the Twilio API when new commit found
--- findjob.py
+++ findjob.py
@@ -1,13 +1,15 @@
 
 import requests
 import json
+import os
+from twilio.rest import Client
+
 
 URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/commits/master"
 
 def getNewDate():
     response = requests.get(url = URL).json()
     return str(response["commit"]["author"]["date"])
-
 
 def getOldDate():
     f = open("date", "r")
@@ -20,12 +22,25 @@ def writeOldDate(newDate):
     f.write(newDate)
     f.close()
 
+def sendText(message):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                .create(
+                     body=message,
+                     from_='+12058436954',
+                     to='+15126952412'
+                 )
+
 def main():
     newDate = getNewDate()
     oldDate = getOldDate()
     
     if newDate > oldDate:
         writeOldDate(newDate)
+        sendText("updated")
         print("updated")
 
 
