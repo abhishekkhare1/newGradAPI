e9bec30c4a387b0f9937c43cd9c5f57a2b7b1199
Abhishek Khare <abhishek.khare@utexas.edu>
2020-08-26 21:32:21 -0500

    Added polling, renamed functions (too used to camelCase from java lol)

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ BIG SHOUTOUT to Pitt Computer Science Club for compiling a list of applications 
 checking if new ones are open, y'all saved us a lot of effort this recruitment cycle
 
 Polls https://github.com/Pitt-CSC/NewGrad-2021 to determine if a new commit has been made
+    - Wanted to utilize Webhooks but didn't own the repository so will set it to poll hourlypip
 
 If updated, sends text with company and url of new application using the Twilio API
+
--- findjob.py
+++ findjob.py
@@ -3,23 +3,27 @@ import requests
 import json
 import os
 import base64
+import logging
+
+import polling2
 from twilio.rest import Client
 
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
 COMMIT_URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/commits/master"
 README_URL = "https://api.github.com/repos/Pitt-CSC/NewGrad-2021/readme"
 REPO_URL = "https://github.com/Pitt-CSC/NewGrad-2021"
 
-
 #Gets date of most recent commit from the repo
-def getNewDate():
+def get_new_date():
     response = requests.get(url = COMMIT_URL).json()
     return str(response["commit"]["author"]["date"])
 
 
 #Makes a request to the repo readme
 #Parses repo to get list of recent company names and links
-def parseReadMe():
+def parse_read_me():
 
     companyList = list()
     linkList = list()
@@ -27,11 +31,10 @@ def parseReadMe():
     #Request README and decode to plaintext
     response = requests.get(url = README_URL).json()
     readme = str(base64.b64decode(response['content']))
-    
     #Cut out closing newLines
-    body,_,_ = readme.rpartition('\\n\\n')
+    body,_,_ = readme.rpartition('\\n')
 
-    lastCompany = getLastCompany()
+    lastCompany = get_last_company()
     body,_,end = body.rpartition('\\n')
     #Get the company name and link to application
     company,_,linkEnd = end.lstrip('| [').partition(']')
@@ -46,13 +49,13 @@ def parseReadMe():
         link,_,_ = linkEnd.lstrip('(').partition(')')
     
     if len(companyList) > 0:
-        setLastCompany(companyList[0])
+        set_last_company(companyList[0])
 
     return companyList, linkList
 
 
 #Reads content from file
-def readFile(fileName):
+def read_file(fileName):
     f = open(fileName, "r")
     content = f.read()
     f.close()
@@ -60,37 +63,38 @@ def readFile(fileName):
 
 
 #Writes content to file
-def writeFile(fileName, content):
+def write_file(fileName, content):
     f = open(fileName, "w")
     f.write(content)
     f.close()
 
 
 #Gets last commit date
-def getOldDate():
-    return readFile('date')
+def get_old_date():
+    return read_file('date')
 
 #Writes the new date to file
-def setOldDate(newDate):
-    writeFile('date', newDate)
+def set_old_date(newDate):
+    write_file('date', newDate)
 
 
 #Gets last company
-def getLastCompany():
-    return readFile('lastCompany')
+def get_last_company():
+    return read_file('lastCompany')
 
 #Writes the most recent company to lastCompany
-def setLastCompany(company):
-    writeFile('lastCompany', company)
+def set_last_company(company):
+    write_file('lastCompany', company)
 
 
 #Texts my number with a message using the Twilio API
-def sendText(message):
+def send_text(message):
     
     if len(message) >= 480:
-        print('error: message length too long')
+        logger.ERROR('message length too long: ' + str(len(message)))
         return
 
+    logger.info('sending text!')
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
@@ -103,30 +107,45 @@ def sendText(message):
                  )
 
 
+def process_commit():
+    logger.info('new commit')
+    companyList, linkList = parse_read_me()
+    length = len(companyList)
+
+    #Avoid excessive Twilio charges in case of mass activity or error
+    if length > 5:
+        massActivity = str(length) + ' New Grad Jobs Posted!\n'
+        send_text(massActivity + REPO_URL)
+        return
+    
+    #Send out texts for each company with link
+    for i in range(length):
+        company = companyList[i]
+        link = linkList[i]
+        message = company + ' added a New Grad role\n' + link
+        send_text(message)
+
+
+#Checks if the most recent commit is newer than the stored one
+def is_new_commit(newDate):
+    oldDate = get_old_date()
+    if newDate <= oldDate:
+        logger.info('no updates')
+    
+    else:
+        set_old_date(newDate)
+        process_commit()
+
+    return False
+
+
 #main
 def main():
-    newDate = getNewDate()
-    oldDate = getOldDate()
-    
-    if newDate > oldDate:
-        setOldDate(newDate)
-        companyList, linkList = parseReadMe()
-        length = len(companyList)
-
-        #Avoid excessive Twilio charges in case of mass activity or error
-        if length > 5:
-            massActivity = str(length) + ' New Grad Jobs Posted!\n'
-            sendText(massActivity + REPO_URL)
-            return
-        
-        #Send out texts for each company with link
-        for i in range(length):
-            company = companyList[i]
-            link = linkList[i]
-            message = company + ' added a New Grad role\n' + link
-            sendText(message)
-    else:
-        print('no updates')
+    polling2.poll(
+    lambda: str(requests.get(url = COMMIT_URL).json()["commit"]["author"]["date"]),
+    check_success=is_new_commit,
+    step=10,
+    poll_forever=True)
 
 
 main()
